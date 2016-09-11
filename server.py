53380a47c5410789eb98121be797278b9df16318
darelswag <bitsydarel@gmail.com>
2016-09-11 20:19:53 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import socket, threading, os, platform
+import socket
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(('', 15555))
 
 while True:
-        os.chdir(platform.system())
         socket.listen(5)
         client, address = socket.accept()
         print "{} connected".format( address )
